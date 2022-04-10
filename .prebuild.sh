23c1551dfda00c9b6c3ce88cfdd1609a9f24608b
ibrah3m <ibrahem.developer@gmail.com>
2022-04-10 18:54:46 +0300

    April 10, 2022 6:54 PM

--- .prebuild.sh
+++ .prebuild.sh
@@ -7,7 +7,7 @@ git clone ${PROJECT_REPO} .
 
 
 ddev config --project-type=$PROJECT_TYPE  &&
-ddev start &&
+ddev start -y &&
 ddev composer install &&
 ddev exec "cat .env.example | sed  -E 's/DB_(HOST|DATABASE|USERNAME|PASSWORD)=(.*)/DB_\1=db/g' > .env" &&
 ddev exec "php artisan key:generate" 
