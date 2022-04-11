dabce8917fea4061b708605e18a5d3ca4c679f60
ibrah3m <ibrahem.developer@gmail.com>
2022-04-11 07:18:13 +0300

    April 11, 2022 7:18 AM

--- .beforebuild.sh
+++ .beforebuild.sh
@@ -1,5 +1,5 @@
- 
-    rm -rf .git &&
-    git clone ${PROJECT_REPO} temp &&
-    mv temp/{,.}* . &&
-    rm -rf temp  
+
+rm -rf .git &&
+git clone ${PROJECT_REPO} temp &&
+mv temp/{,.}* . &&
+rm -rf temp  
