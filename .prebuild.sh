bf28fcdfa114fe41f2d08b3424b3a3f195eaaa55
ibrah3m <ibrahem.developer@gmail.com>
2022-04-11 06:38:10 +0300

    April 11, 2022 6:38 AM

--- .prebuild.sh
+++ .prebuild.sh
@@ -5,5 +5,4 @@ ddev start -y &&
 ddev composer install &&
 ddev exec "cat .env.example | sed  -E 's/DB_(HOST|DATABASE|USERNAME|PASSWORD)=(.*)/DB_\1=db/g' > .env" &&
 ddev exec "php artisan migrate" &&
-ddev exec "php artisan key:generate"  &&
-gp sync-done prebuild
+ddev exec "php artisan key:generate"  
