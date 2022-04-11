28106fd8560cbf01c1a6c8c25fda115f3093d93a
ibrah3m <ibrahem.developer@gmail.com>
2022-04-11 03:53:25 +0300

    April 11, 2022 3:53 AM

--- .prebuild.sh
+++ .prebuild.sh
@@ -4,4 +4,5 @@ ddev config --project-type=$PROJECT_TYPE  &&
 ddev start -y &&
 ddev composer install &&
 ddev exec "cat .env.example | sed  -E 's/DB_(HOST|DATABASE|USERNAME|PASSWORD)=(.*)/DB_\1=db/g' > .env" &&
+ddev exec "php artisan migrate" 
 ddev exec "php artisan key:generate" 
