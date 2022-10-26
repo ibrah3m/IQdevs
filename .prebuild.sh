ab20bbf097577ea8980fe53eb48b92e0033d77c5
ibrah3m <91458548+ibrah3m@users.noreply.github.com>
2022-10-26 21:55:28 +0300

    Update .prebuild.sh
    moving some lines to before :D

--- .prebuild.sh
+++ .prebuild.sh
@@ -1,6 +1,6 @@
 # brew install drud/ddev/ddev	&&																						
 sudo apt-get update && sudo apt-get install -y xdg-utils &&
-ddev config --project-type=$PROJECT_TYPE  &&
+# ddev config --project-type=$PROJECT_TYPE  &&
 ddev start -y &&
 ddev composer install &&
 ddev exec "cat .env.example | sed  -E 's/DB_(HOST|DATABASE|USERNAME|PASSWORD)=(.*)/DB_\1=db/g' > .env" &&
