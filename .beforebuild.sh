49bd5b15a5b8111bdedac3370e4b06679729d09f
ibrah3m <91458548+ibrah3m@users.noreply.github.com>
2022-10-28 20:58:01 +0300

    Update .beforebuild.sh

--- .beforebuild.sh
+++ .beforebuild.sh
@@ -1,4 +1,6 @@
 rm -rf .git &&
+
+# add condition check variable exist or not if not create fake config.yml
 git clone ${PROJECT_REPO} temp &&
 find temp -maxdepth 1 -mindepth 1 -print0 | xargs -0 -I {} mv {} . &&
 rm -rf temp  &&
